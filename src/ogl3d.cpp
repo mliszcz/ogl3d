10462319c936891b70127dfdb675eded402c2c47
mliszcz <liszcz.michal@gmail.com>
2014-07-25 22:34:32 +0100

    eclipse project created

---
+++ src/ogl3d.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ogl3d.cpp
+// Author      : michal
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hello world!" << endl; // prints
+	return 0;
+}
